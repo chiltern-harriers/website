1901e4afd93b49eb1e6af709ab912d466c536a25
Matt Channer <matt.channer@referencepoint.co.uk>
2019-05-04 19:11:30 +0100

    Added bootstrap sass files Sass bundle optimizer Bundle config to minify and compile scss files to css

---
+++ src/Harriers.Web/Components/BundlingComponent.cs
@@ -0,0 +1,37 @@
+﻿using BundleTransformer.Core.Bundles;
+using BundleTransformer.Core.Resolvers;
+using System.Web.Optimization;
+using Umbraco.Core.Composing;
+
+namespace Harriers.Web.Components
+{
+    public class BundlingComponent : IComponent
+    {
+        public void Initialize()
+        {
+            RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void Terminate()
+        {
+        }
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+            BundleResolver.Current = new CustomBundleResolver();
+
+            bundles.Add(new CustomStyleBundle("~/bundles/styles")
+                .Include(
+                    "~/Content/core.scss",
+                    "~/Content/fonts.css"));
+
+            bundles.Add(new CustomScriptBundle("~/bundles/scripts")
+                .Include(
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/popper.js",
+                    "~/Scriptsbootstrap.js"));
+        }
+    }
+}
---
+++ src/Harriers.Web/Components/ComponentComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Harriers.Web.Components
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class ComponentComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<BundlingComponent>();
+        }
+    }
+}
